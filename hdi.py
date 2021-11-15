ffec01a49675d049e46c825b2dbd931c873cd0e2
Bypass Akun <94339889+Bypassakun365@users.noreply.github.com>
2021-11-15 12:58:33 +0700

    Update hdi.py

--- hdi.py
+++ hdi.py
@@ -1,10 +1,10 @@
-/bin/bash
-echo : Higgs domino island
+#/bin/bash
+#echo : Higgs domino island
 domino island
 scatter
 super win 4.600.000.000
 mega win 800.000.000
-#big win 300.000.000
+big win 300.000.000
 ============================>
 ID :
 PASSWORD :
